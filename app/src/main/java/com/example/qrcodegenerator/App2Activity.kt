063788b03707836e1382a2d5b4ad49d52d3f8b4a
rdasrituraj17 <rdasrituraj@gmail.com>
2020-11-13 02:21:43 +0530

    Initial commit

---
+++ app/src/main/java/com/example/qrcodegenerator/App2Activity.kt
@@ -0,0 +1,12 @@
+package com.example.qrcodegenerator
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class App2Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?)
+    {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main2)
+    }
+}
