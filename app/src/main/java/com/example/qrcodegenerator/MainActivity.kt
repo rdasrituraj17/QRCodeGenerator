c8ce7fef18032cbae330e8b3ac66eb31ab18af61
rdasrituraj17 <rdasrituraj@gmail.com>
2020-11-13 15:15:17 +0530

    updated

---
+++ app/src/main/java/com/example/qrcodegenerator/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.example.qrcodegenerator
+
+import android.app.Activity
+import android.content.Context
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+
+class MainActivity : Activity() {
+    var button: Button? = null
+    public override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        addListenerOnButton()
+    }
+    fun addListenerOnButton() {
+        val context1: Context = this
+        button = findViewById<View>(R.id.scan) as Button
+        button!!.setOnClickListener {
+            val intent = Intent(context1, Scan::class.java)
+            startActivity(intent)
+        }
+        val context: Context = this
+        button = findViewById<View>(R.id.gen) as Button
+        button!!.setOnClickListener {
+            val intent = Intent(context, Generate::class.java)
+            startActivity(intent)
+        }
+    }
+}
---
+++ app/src/main/java/com/example/qrcodegenerator/Scan.kt
@@ -0,0 +1,12 @@
+package com.example.qrcodegenerator
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Scan : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?)
+    {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_scan)
+    }
+}
